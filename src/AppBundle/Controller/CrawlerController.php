48dc5e281e57196bc7d9003597fed2fb72f1e762
Maciek <maciej.sikorski@valueadd.pl>
2017-10-14 12:35:09 +0200

    Crawler in composer

---
+++ src/AppBundle/Controller/CrawlerController.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Controller;
+
+class CrawlerController extends BaseController
+{
+
+    public function testAction()
+    {
+        die('elo');
+    }
+}
