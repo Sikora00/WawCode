8aba5286896387060619e3f5819542d98f01132c
Maciek <maciej.sikorski@valueadd.pl>
2017-10-14 12:59:52 +0200

    Test request

--- src/AppBundle/Controller/CrawlerController.php
+++ src/AppBundle/Controller/CrawlerController.php
@@ -3,11 +3,28 @@ declare(strict_types=1);
 
 namespace AppBundle\Controller;
 
+use Goutte\Client;
+
 class CrawlerController extends BaseController
 {
 
     public function testAction()
     {
         die('elo');
+    }
+
+    public function downloadDataAction()
+    {
+        $client = new Client();
+        $crawler = $client->request('GET', 'https://github.com/login');
+
+        // select the form and fill in some values
+        $form = $crawler->selectButton('Sign in')->form();
+        $form['login'] = 'symfonyfan';
+        $form['password'] = 'anypass';
+
+// submit that form
+        $crawler = $client->submit($form);
+
     }
 }
